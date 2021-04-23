78fedcfbe18e547d045d3346fa07af501563d351
Win7yes <francodmeneguzzi@gmail.com>
2021-04-23 00:40:26 -0300

    Remove manual testing stuff

--- Program.cs
+++ Program.cs
@@ -9,30 +9,12 @@ namespace Chess
         static Piece whiteKing;
         static void Main()
         {
-            pawn = new Piece();
-            king = new Piece(PieceType.King);
-            whiteKing = new Piece(PieceType.King, true);
-
-            PrintPieces();
-
-            bool a = pawn.Move(1, 0);
-            bool b = king.Move(5, 3);
-            bool c = whiteKing.Move(2, 9);
-
-            Console.WriteLine($"{a} {b} {c}");
-            PrintPieces();
-
             Console.WriteLine("Board: \n");
 
             Board board = new();
 
             board.StartBoard();
             board.Draw();
-        }
-
-        static void PrintPieces()
-        {
-            Console.WriteLine($"{pawn}\n{king}\n{whiteKing}");
         }
     }
 }
